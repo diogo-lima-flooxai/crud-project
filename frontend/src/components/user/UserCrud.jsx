fee85828abff1bed4f5e84ed413a945cb30dc641
diogo <diogo.lima.flooxai@gmail.com>
2025-05-14 08:41:00 -0300

    feat: add functions to the form and pass data to the db

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,50 +1,104 @@
-import React, { Component } from 'react'; 
-import axios from 'axios';
-import Main from '../template/Main';
+import React, { Component } from "react";
+import axios from "axios";
+import Main from "../template/Main";
 
 const headerProps = {
-    icon: 'users',
-    title: 'Usuários',
-    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
-}
+  icon: "users",
+  title: "Usuários",
+  subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir!",
+};
 
-const baseUrl = 'http://localhost:3001/users';
+const baseUrl = "http://localhost:3001/users";
 const initialState = {
-    user: { name: '', email: '' },
-    list: []
-}
+  user: { name: "", email: "" },
+  list: [],
+};
 
-export default class UserCrud extends Component { 
+export default class UserCrud extends Component {
+  state = { ...initialState };
 
-    state = { ...initialState }
+  clear() {
+    this.setState({ user: initialState.user });
+  }
 
-    clear() {
-        this.setState({ user: initialState.user })
-     }
+  save() {
+    const user = this.state.user;
+    const method = user.id ? "put" : "post";
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
 
-    save() {
-        const user = this.state.user
-        const method = user.id ? 'put' : 'post'
-        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+    axios[method](url, user).then((resp) => {
+      const list = this.getUpdatedList(resp.data);
+      this.setState({ user: initialState.user, list });
+    });
+  }
 
-        axios[method](url, user)
-            .then(resp => {
-                const list = this.getUpdatedList(resp.data)
-                this.setState({ user: initialState.user, list })
-            })
-    }
+  getUpdatedList(user) {
+    const list = this.state.list.filter((u) => u.id !== user.id);
+    list.unshift(user);
+    return list;
+  }
 
-    getUpdatedList(user) {
-        const list = this.state.list.filter(u => u.id !== user.id)
-        list.unshift(user)
-        return list
-    }
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.user.name}
+                onChange={(e) => this.updateField(e)}
+                placeholder="Digite o nome..."
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                value={this.state.user.email}
+                onChange={(e) => this.updateField(e)}
+                placeholder="Digite o e-mail..."
+              />
+            </div>
+          </div>
+        </div>
 
-    render() {
-        return (
-            <Main {...headerProps}>
-                Cadastro de Usuario
-            </Main>
-        )
-    }
+        <hr />
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button className="btn btn-primary" onClick={(e) => this.save(e)}>
+              Salvar
+            </button>
+            <button
+              className="btn btn-secondary ml-2"
+              onClick={(e) => this.clear(e)}
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  updateField(event) {
+    const user = { ...this.state.user };
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  }
+
+  render() {
+    return (
+        <Main {...headerProps}>
+            {this.renderForm()}
+        </Main>
+    )       
+  }
 }
